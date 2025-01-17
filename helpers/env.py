e005e6d602ad21406d7628d1e816d304b7298649
humaidhusain98 <humaidhusain98@gmail.com>
2025-01-17 14:32:55 +0530

    Removing credentials and pushing to git

--- helpers/env.py
+++ helpers/env.py
@@ -2,6 +2,10 @@ import os
 localUrl = os.getenv('localUrl')
 prodUrl = os.getenv('prodUrl')
 isDev = os.getenv('isDev')
+aws_secret_id = os.getenv('AWS_SECRET_KEY_ID')
+aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+s3_bucket_url = os.getenv('S3_BUCKET_URL')
+
 
 if isDev=="True":
     url = prodUrl
--- helpers/s3Bucket.py
+++ helpers/s3Bucket.py
@@ -1,4 +1,5 @@
 import boto3
+from env import aws_secret_id,aws_secret_key,s3_bucket_url
 SUPPORTED_FILE_TYPES = {
     'image/png':'png',
     'image/jpg':'jpg',
@@ -8,8 +9,8 @@ SUPPORTED_FILE_TYPES = {
 AWS_BUCKET = 'python-opencv-bucket'
 
 session = boto3.Session(
-    aws_access_key_id="AKIA37W4OTURBXKBHHG4",
-    aws_secret_access_key="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",
+    aws_access_key_id=aws_secret_id,
+    aws_secret_access_key=aws_secret_key,
 )
 
 s3 = session.resource('s3')
@@ -18,4 +19,4 @@ bucket = s3.Bucket(AWS_BUCKET)
 async def s3_upload(contents: bytes,key: str):
     print(f'Uploading {key} to s3')
     bucket.put_object(Key=key, Body=contents)
-    return {"url":"https://python-opencv-bucket.s3.ap-south-1.amazonaws.com/"+key}
+    return {"url":s3_bucket_url+"/"+key}
